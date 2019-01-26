c8b41d70f7187f80a3aa979608ce120b48b0d85e
Trey Shedrick <treyshedrick@gmail.com>
2019-01-26 13:38:15 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+
+function p1()
+{
+/*Use lines 5-17 if exam 2
+	   d=document.f1.ta1.value;
+	   cr=String.fromCharCode(10);
+	   tb=String.fromCharCode(9);
+	   lf=String.fromCharCode(13);
+	   while (d.indexOf(cr)>-1)
+	          d=d.replace(cr,"");
+	   while (d.indexOf(lf)>-1)
+	          d=d.replace(lf,"");
+	   while (d.indexOf(tb)>-1)
+	          d=d.replace(tb,"");
+	   parser=new DOMParser();
+	   xmlDoc=parser.parseFromString(d,"text/xml");
+	   root=xmlDoc.documentElement;
+*/
+//Exam answers start here
+	  
+	   
+}
+
+function p2()
+{
+	
+}
